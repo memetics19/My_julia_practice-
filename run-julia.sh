97dba6f854f1ca954f7a5bbc37c310850bf9289c
memetics19 <shreeda.bhat@yahoo.com>
2020-09-25 18:02:34 +0530

    shell script can be used to run the julia program it's working fine

--- run-julia.sh
+++ run-julia.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 cd Basics
-julia arithmaticexpression.julia
+julia arithmaticexpression.jl
 julia datatypes.jl
-julia function.jl
+julia functions.jl
 julia Helloworld.jl
 julia if-condition.jl
-julia logicalexpres.jl
+julia logicalexpressions.jl
+julia loops-for.jl
+julia loops-while.jl
+# julia plot.jl
+julia strings.jl
+julia variables.jl
+julia DataStructures.jl

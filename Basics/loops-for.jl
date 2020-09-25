18497ecba57bb5e1757afacefad3a731995c1411
memetics19 <shreeda.bhat@yahoo.com>
2020-09-25 17:51:20 +0530

    updated for loops

--- Basics/loops-for.jl
+++ Basics/loops-for.jl
@@ -24,6 +24,48 @@ for i in x
     println("what i'm passing now is $i")
 end
 
+
+
 #= 
 Note: in the condition it should be `variable = iterable`
 =#
+
+println("***********************3rd Example*************************")
+
+for i in 1:10
+    println(i)
+end
+
+
+println("***********************4th Example*************************")
+
+my_bio = ["shreeda","20","Astaqc Consulting"]
+
+for bio in my_bio
+    println("let's see this array $bio")
+end
+
+println("*********************** 5th Example*************************")
+m,n = 3,3 
+array = zeros(m,n)
+println(array)
+
+for i in 1:m
+    for j in 1:n
+        array[i,j]=i+j
+    end
+end
+println(array)
+
+println("*********************** 6th Example*************************")
+# Let's do list comprehension
+B = zeros(3,3)
+for i in 1:m,j in 1:n
+    B[i,j]=i+j
+end
+println(B)
+
+println("*********************** 7th Example*************************")
+# Sorry its array comprehension
+c= [i+j for i in 1:n, j in 1:m]
+print(c)
